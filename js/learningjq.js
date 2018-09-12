942e2677ac39191fb62a5abcdd7de7b428540797
gwalrod <gwalrod@nerdery.com>
2018-09-11 20:14:41 -0500

    home 9/11

--- js/learningjq.js
+++ js/learningjq.js
@@ -120,3 +120,94 @@ $('input[name=email]').on('keyup', function(){
     }
 
 });
+
+//adding classes
+
+   $('#text').find('p').addClass('HAH');
+
+    wQuery(".my-selector").addClass("MYNEWCLASS");
+
+    wQuery("#context").addClass("HELLO");    
+
+//contect menu
+
+    $(document).on('contextmenu', function(){
+        return false;
+    });
+
+//prevent a link
+
+$('[href="https://google.com"]').on('click', function(event){
+    console.log("linking to google?");
+    event.preventDefault();
+    return false;
+});
+
+//submenu consoles
+
+console.log($('.submenu a').first().text());
+console.log($('.submenu a').last().text());
+
+//buttons
+
+
+$('#prepend, #append, #replace').on('click', function(e){
+    e.preventDefault();
+
+    var el = $(e.currentTarget);
+    var action = el.attr('id');
+    var content = $('.text').val();
+
+    if(action == "prepend"){
+        console.log("Prepending...");
+        $('#text').prepend(content);
+
+    }else if(action == "append"){
+        console.log("Appending...");
+        $('#text').append(content);
+
+
+    }else if(action == "replace"){
+        console.log("Replacing...");
+        $('#text').html(content);
+
+
+    }
+
+    $('text').val('');
+
+});
+
+//focusin focusout
+
+$('textarea').focusin(function(){
+    console.log("Focused in on the text area");
+});
+
+$('textarea').focusout(function(){
+    console.log("Text area has lost focus");
+});
+
+$('input').focusout(function(){
+   if($(this).val().indexOf('@') > -1 && $(this).val().indexOf('.') > -1){
+       $('.status').html("Valid Email");
+   }else{
+       $('.status').html("Your email is invalid");
+   }
+});
+
+//contains
+
+$('p:contains("Lorem")').html("This had Lorem in it and yet it still does. ");
+
+$('p').each(function(){
+    console.log($(this).text());
+});
+
+$('p').each(printThis);
+
+console.log($('p').text());
+
+if($(':contains("Lorem")').is("p")){
+    console.log("Lorem is inside paragraph");
+}
--- js/main.js
+++ js/main.js
@@ -22,27 +22,17 @@ btn.addEventListener ("click", buttonClicked);
 
 
 $(document).ready(function(){
+
+    function printThis(el){
+        if($(this).text()) {
+            console.log($(this).text());
+        }else{
+            console.log($(this).val());
+        }
+    }
+
     var el = document.getElementById('text');
 
-    console.log($('.submenu a').first().text());
-    console.log($('.submenu a').last().text());
-
-
-    //$('#text').find('p').addClass('HAH');
-
-    //wQuery(".my-selector").addClass("MYNEWCLASS");
-
-    //wQuery("#context").addClass("HELLO");    
-
-    /*$(document).on('contextmenu', function(){
-        return false;
-    });*/
-
-    $('[href="https://google.com"]').on('click', function(event){
-        console.log("linking to google?");
-        event.preventDefault();
-        return false;
-    });
 
     $('[data-trigger="dropdown"]').on('mouseenter', function(){
         var submenu = $(this).parent().find('.submenu');
@@ -54,49 +44,15 @@ $(document).ready(function(){
 
     });
 
-    $('#prepend, #append, #replace').on('click', function(e){
-        e.preventDefault();
+    $('input').focusin(printThis);
 
-        var el = $(e.currentTarget);
-        var action = el.attr('id');
-        var content = $('.text').val();
-
-        if(action == "prepend"){
-            console.log("Prepending...");
-            $('#text').prepend(content);
-
-        }else if(action == "append"){
-            console.log("Appending...");
-            $('#text').append(content);
-
-
-        }else if(action == "replace"){
-            console.log("Replacing...");
-            $('#text').html(content);
-
-
-        }
-
-        $('text').val('');
-
+    $('input').css({
+        background: '#f00',
+        padding: '10px',
+        borderColor: '#000',
+        backgroundSize: 'cover'
     });
 
-
-    $('textarea').focusin(function(){
-        console.log("Focused in on the text area");
-    });
-
-    $('textarea').focusout(function(){
-        console.log("Text area has lost focus");
-    });
-
-   $('input').focusout(function(){
-       if($(this).val().indexOf('@') > -1 && $(this).val().indexOf('.') > -1){
-           $('.status').html("Valid Email");
-       }else{
-           $('.status').html("Your email is invalid");
-       }
-   });
 
 
 });
